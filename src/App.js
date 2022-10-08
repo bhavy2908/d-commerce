b7efdf4dc0b6abfe64250b5be46f6446da658ac2
Bhavy <bhavy2908@gmail.com>
2022-10-08 14:20:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Navb from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hero from './components/Carousel';
+import Products from './components/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: '#F2F2F2'}}>
+      <Navb />
+      <Hero />
+      <div className='container'>
+        <Products />
+      </div>
     </div>
   );
 }
