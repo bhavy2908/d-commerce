70ceb2ddd0cbad32571a6cd528a3270922336c4e
Bhavy <bhavy2908@gmail.com>
2022-10-11 23:27:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Navb from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './components/Carousel';
 import Products from './components/Products';
+import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
-    <div style={{ backgroundColor: '#DDDDDD'}}>
-      <Navb />
-      <Hero />
-      <div className='container'>
-        <Products />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
