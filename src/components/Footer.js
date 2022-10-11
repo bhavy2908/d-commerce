41c5b3ddf942a01fe0c56029447aa82a3a471428
Bhavy <bhavy2908@gmail.com>
2022-10-11 23:26:16 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import logo from '../assets/logos/logo.png'
+
+function Footer() {
+    return (
+        <div>
+            
+            <div style={{ backgroundColor: '#444444', color: 'white' , textAlign: 'center'}} className='py-4'>
+                Copyright © 2022 <b>Bhavy</b>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default Footer;
